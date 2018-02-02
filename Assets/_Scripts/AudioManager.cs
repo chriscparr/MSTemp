c0a7f18b4341131799fa820959bf1bf44e7e02c0
Chris Parr <chris.parr@mindshareworld.com>
2018-02-02 17:33:43 +0000

    Created basic templates for several manager classes

---
+++ Assets/_Scripts/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	public static AudioManager Instance {get { return s_instance;}}
+	private static AudioManager s_instance = null;
+
+	private void Awake()
+	{
+		s_instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/_Scripts/ModelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelManager : MonoBehaviour {
+
+	public static ModelManager Instance {get { return s_instance;}}
+	private static ModelManager s_instance = null;
+
+	private void Awake()
+	{
+		s_instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
