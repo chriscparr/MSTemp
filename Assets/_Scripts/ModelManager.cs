4d6e68c77eb60999cc6c506e70886738ee35fb1d
Chris Parr <chris.parr@mindshareworld.com>
2018-02-05 13:38:52 +0000

    Created and populated references for model prefabs and gameobject to contain all elements of the 3d model

--- Assets/_Scripts/ModelManager.cs
+++ Assets/_Scripts/ModelManager.cs
@@ -4,22 +4,60 @@ using UnityEngine;
 
 public class ModelManager : MonoBehaviour 
 {
+	[SerializeField]
+	private GameObject m_mainContainerPrefab;
+	[SerializeField]
+	private GameObject m_subcellPrefab;
+	[SerializeField]
+	private GameObject m_modelRoot;
 
 	public static ModelManager Instance {get { return s_instance;}}
 	private static ModelManager s_instance = null;
+
+	private GameObject m_mainContainer;
+	private List<Subcell> m_subcells = new List<Subcell>();
 
 	private void Awake()
 	{
 		s_instance = this;
 	}
 
-	// Use this for initialization
-	void Start () {
-		
+	private void Start()
+	{
+		InitialiseModel (GenerateDebugPresentation ());
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+
+	public void InitialiseModel(PresentationData a_pData)
+	{
+		//m_mainContainer = 
+
+	}
+
+
+
+
+
+	private PresentationData GenerateDebugPresentation()
+	{
+		string testData = "{\n\"PresenterName\":\"Mr. Tester\",\n\"PresenterPosition\":\"Test Manager\",\n\"ClientName\":\"IndustryLeader Inc.\",\n\"Industries\":[\"Finance\", \"Retail\", \"World Domination\"],\n\"Markets\":[\"Europe\", \"Asia\", \"America\"],\n\"Notes\":[\"Note 1\", \"Note 2\", \"Note 3\"],\n\"Services\":\n[\n{\"ServiceName\":\"FAST\",\"ServiceWeighting\":1.0,\"ServiceIntroText\":\"Introduction Text!\",\"ServiceVideoPaths\":[]},\n{\"ServiceName\":\"SHOP\",\"ServiceWeighting\":1.0,\"ServiceIntroText\":\"Introduction Text!\",\"ServiceVideoPaths\":[]},\n{\"ServiceName\":\"GROWTH\",\"ServiceWeighting\":1.0,\"ServiceIntroText\":\"Introduction Text!\",\"ServiceVideoPaths\":[]},\n{\"ServiceName\":\"DATA\",\"ServiceWeighting\":1.0,\"ServiceIntroText\":\"Introduction Text!\",\"ServiceVideoPaths\":[]},\n{\"ServiceName\":\"LOOP\",\"ServiceWeighting\":1.0,\"ServiceIntroText\":\"Introduction Text!\",\"ServiceVideoPaths\":[]},\n{\"ServiceName\":\"CONTENT\",\"ServiceWeighting\":1.0,\"ServiceIntroText\":\"Introduction Text!\",\"ServiceVideoPaths\":[]},\n{\"ServiceName\":\"AGILE\",\"ServiceWeighting\":1.0,\"ServiceIntroText\":\"Introduction Text!\",\"ServiceVideoPaths\":[]},\n{\"ServiceName\":\"LIFE\",\"ServiceWeighting\":1.0,\"ServiceIntroText\":\"Introduction Text!\",\"ServiceVideoPaths\":[]}\n]\n}";
+		PresentationData pData = JsonUtility.FromJson<PresentationData> (testData);
+		return pData;
+	}
+
+
+	private void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Alpha1))
+		{
+			PresentationData presData = GenerateDebugPresentation ();
+			Debug.Log (
+				"<color=#aaaa00>" +
+				"PresenterName:" + presData.PresenterName.ToString() + ",\n" +
+				"PresenterPosition:" + presData.PresenterPosition.ToString() + ",\n" +
+				"ClientName:" + presData.ClientName.ToString() + ",\n" +
+				"</color>"
+			);
+		}
 	}
 }
