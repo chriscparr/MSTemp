220a21ed03d4719fcb06783707a5bb8187dbb14e
Chris Parr <chris.parr@mindshareworld.com>
2018-02-05 10:20:58 +0000

    added json schema definitions for presentation data

---
+++ Assets/Data/PresentationData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class PresentationData
+{
+	public string PresenterName;
+	public string PresenterPosition;
+	public string ClientName;
+	public string[] Industries;
+	public string[] Markets;
+	public string[] Notes;
+	public ServiceData[] Services;
+}
