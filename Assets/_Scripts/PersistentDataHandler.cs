0a55bfaf10db205b2a511aab2d5d493d67433895
Chris Parr <chris.parr@mindshareworld.com>
2018-02-05 11:11:30 +0000

    Added a class for handling persistent data (json definition for presentation data). I will replace this with tested code from a previous project when I get home tonight

---
+++ Assets/_Scripts/PersistentDataHandler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class PersistentDataHandler : MonoBehaviour 
+{
+
+	//Replace this with code from previous work
+
+	public void SaveJSON(string a_jsonStr, string a_fileName)
+	{
+		string path = Application.persistentDataPath + "/" + a_fileName + ".json";
+		if (!File.Exists (path))
+		{
+			StreamWriter writer = new StreamWriter (path, false);
+			writer.Write (a_jsonStr);
+			writer.Close ();
+		} 
+		else
+		{
+			throw new FileLoadException ("File already exists!");
+		}
+	}
+	/*
+	public PresentationData[] LoadPresentationData()
+	{
+		List<PresentationData> presentations = new List<PresentationData> ();
+		string path = Application.persistentDataPath;
+		DirectoryInfo d = new DirectoryInfo (path);
+		FileInfo[] files = d.GetFiles ();
+
+		int jsonCount = 0;
+		foreach (FileInfo finfo in files)
+		{
+			if (finfo.Extension.Contains ("json"))
+			{
+				jsonCount++;
+
+			}
+		}
+
+	}
+	*/
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
