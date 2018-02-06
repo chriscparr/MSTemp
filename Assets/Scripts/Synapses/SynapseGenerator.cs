da88f51318c8e5b4c332aa6c09f77d096e4d7300
Alastair Collins <Alastair.Collins@ad.insidemedia.net>
2018-02-06 16:34:45 +0000

    Feature that generates customisable 3D lines- intended to be used inside Subcells (see UI designs)

---
+++ Assets/Scripts/Synapses/SynapseGenerator.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SynapseGenerator : MonoBehaviour {
+
+	public GameObject SynapsePrefab;
+	public int NumberOfSynapsesToGenerate = 3;
+	public GameObject BoundingSphere; // this should be the clickable Subcell
+	public float MinimumRadiusRandomisation = 2.5f;
+	public float MaximumRadiusRandomisation = 5f;
+
+	public float MinimumDistanceRandomisation = -10;
+	public float MaximumDistanceRandomisation = 10;
+
+
+	public bool shouldIEndWhereIBegin = true;
+
+	public int minimumNumberOfNodes = 4;
+	public int maximumNumberOfNodes = 10;
+
+	iTweenPath pathData;
+	List<iTweenPath> pathCollections = new List<iTweenPath> ();
+
+	const string glyphs= "abcdefghijklmnopqrstuvwxyz";
+	int charAmount;
+
+	// Use this for initialization
+	void Start () {
+
+		charAmount = Random.Range(5,10);
+
+		for (int i = 0; i < NumberOfSynapsesToGenerate; i++) {
+			GameObject currentSynapse = Instantiate (SynapsePrefab, transform.position, Quaternion.identity) as GameObject;
+			pathData = currentSynapse.GetComponent<iTweenPath> ();
+
+			pathData.nodeCount = Random.Range (minimumNumberOfNodes, maximumNumberOfNodes);
+
+			pathCollections.Add (pathData);
+			}
+
+		foreach (iTweenPath path in pathCollections)
+		{
+			AssignNodes (path);
+			// Cleanup (path);
+			StartPath(path);
+		}
+	}
+	
+	
+	// Update is called once per frame
+	Vector3 GetRandomDirection () {
+		Vector3 direction = Random.insideUnitSphere * GetRandomDistance ();
+		return direction;
+
+	}
+
+	float GetRandomDistance() {
+		return Random.Range (MinimumDistanceRandomisation, MaximumDistanceRandomisation);
+	}
+
+	void AssignNodes (iTweenPath p )
+	{
+		for (int n = 0; n < p.nodeCount; n++) {
+
+			Vector3 veryStartingPoint = BoundingSphere.GetComponent<Renderer>().bounds.center;
+
+			p.nodes [n] = veryStartingPoint;
+
+			if (n != 0) {
+				
+				Vector3 dist = (GetRandomDirection () * GetRandomDistance ());
+
+				p.nodes [n] += dist;
+
+				if (!BoundingSphere.GetComponent<Renderer>().bounds.Contains (p.nodes [n])) {
+					// ???
+					p.nodes [n] = veryStartingPoint;
+					p.nodes [n] = Random.insideUnitSphere * GetRandomDistance ();
+				}
+			}
+
+		}
+
+		if (shouldIEndWhereIBegin == true) {
+			p.nodes [(p.nodeCount-1)] = p.nodes [0];
+		}
+	}
+		
+
+	void StartPath (iTweenPath p)
+	{
+		string s = "";
+
+		for (int i = 0; i < charAmount; i++) {
+			s += glyphs [Random.Range (0, glyphs.Length)]; 
+		}
+		p.pathName = s;
+		Debug.Log (p.pathName);
+		p.GetComponent<SynapseMover> ().Tweener (p);
+	}
+}
---
+++ Assets/Scripts/Synapses/SynapseMover.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SynapseMover : MonoBehaviour {
+	bool allowMove;
+	float val = 0;
+	float velocity = 1;
+	Vector3[] arr;
+
+	public float maxTime = 30;
+
+	// Use this for initialization
+	public void Tweener(iTweenPath p) {
+		arr = p.nodes.ToArray ();
+		allowMove = true;
+		// iTween.MoveTo (gameObject, iTween.Hash ("path", iTweenPath.GetPath(p.pathName), "time", (Speed), "orientToPath", true, "looktime", 2f, "oncomplete", "Reset", "looptype", iTween.LoopType.pingPong, "easetype", iTween.EaseType.easeInOutCubic));
+	}
+
+	void Awake() {
+		iTween.Init (this.gameObject);
+	}
+
+	void Update() {
+		if (allowMove)
+			val = Mathf.SmoothStep(0, 1, Mathf.PingPong(Time.time/maxTime, 1));
+	
+		iTween.PutOnPath (this.gameObject, arr, val);
+
+		if (val >= 0.9f) {
+			Debug.Log ("DISABLE THE MOVER");
+			this.enabled = false;
+		}
+
+	
+	}
+}
