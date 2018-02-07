ec4941dfd9fb4759144d309e2ed15703958dc6a8
Chris Parr <chris.parr@mindshareworld.com>
2018-02-07 16:08:52 +0000

    Prefab + script for selecting from saved presentations

---
+++ Assets/_Scripts/UI/SelectSavedPresentationView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectSavedPresentationView : MonoBehaviour 
+{
+	[SerializeField]
+	private Button m_presentationOneButton;
+
+
+	private void OnPresentationOneButtonPressed()
+	{
+		//UIManager - display next screen
+		gameObject.SetActive(false);
+	}
+
+	private void OnEnable()
+	{
+		m_presentationOneButton.onClick.AddListener (OnPresentationOneButtonPressed);
+	}
+
+	private void OnDisable()
+	{
+		m_presentationOneButton.onClick.RemoveListener (OnPresentationOneButtonPressed);
+	}
+}
