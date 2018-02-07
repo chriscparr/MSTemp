6847e3de3bc9c0f43e07afed4c1b9cd6303736ef
Chris Parr <chris.parr@mindshareworld.com>
2018-02-07 16:15:43 +0000

    Prefab + script for creating new presentations

---
+++ Assets/_Scripts/UI/NewPresentationView.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewPresentationView : MonoBehaviour 
+{
+	[SerializeField]
+	private Button m_savePresentationButton;
+
+	private void OnSavePresentationButtonPressed()
+	{
+		//UIManager - display next screen
+		gameObject.SetActive(false);
+	}
+
+	private void OnEnable()
+	{
+		m_savePresentationButton.onClick.AddListener (OnSavePresentationButtonPressed);
+	}
+
+	private void OnDisable()
+	{
+		m_savePresentationButton.onClick.RemoveListener (OnSavePresentationButtonPressed);
+	}
+}
