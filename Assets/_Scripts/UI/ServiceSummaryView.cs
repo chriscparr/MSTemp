0abdbacc5cd07ff997c56a988298fa2b5c29fb4c
Chris Parr <chris.parr@mindshareworld.com>
2018-02-12 11:07:38 +0000

    Created service ui prefab + script to show service specific information when a service is selected in the model

---
+++ Assets/_Scripts/UI/ServiceSummaryView.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ServiceSummaryView : MonoBehaviour 
+{
+	[SerializeField]
+	private Button m_exitButton;
+	[SerializeField]
+	private Text m_titleText;
+	[SerializeField]
+	private Text m_introText;
+
+	public void SetupServiceView(ServiceData a_sData)
+	{
+		m_titleText.text = a_sData.ServiceName;
+		m_introText.text = a_sData.ServiceIntroText;
+	}
+
+	private void OnEnable()
+	{
+		m_exitButton.onClick.AddListener (OnFinishButtonPressed);
+	}
+
+	private void OnDisable()
+	{
+		m_exitButton.onClick.RemoveListener (OnFinishButtonPressed);
+	}
+
+	private void OnFinishButtonPressed()
+	{
+		gameObject.SetActive (false);
+	}
+}
