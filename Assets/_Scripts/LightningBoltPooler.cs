5585e82c7989d2656f9738289b1fb1071fdad499
Chris <ccparr13@gmail.com>
2018-02-18 16:20:55 +0000

    Tidied up pooling script and added initialisation checks in publicly available functions

--- Assets/_Scripts/LightningBoltPooler.cs
+++ Assets/_Scripts/LightningBoltPooler.cs
@@ -7,8 +7,8 @@ public class LightningBoltPooler : MonoBehaviour
 	[SerializeField]
 	private GameObject m_lightningBoltPrefab;
 
-	private float m_lb_MinDuration = 0.05f;
-	private float m_lb_MaxDuration = 0.5f;
+	private float m_lb_MinDuration = 0.5f;
+	private float m_lb_MaxDuration = 1f;
 	private float m_lb_MinChaos = 0.15f;
 	private float m_lb_MaxChaos = 0.5f;
 	private int m_lb_MinGenerations = 4;
@@ -20,44 +20,55 @@ public class LightningBoltPooler : MonoBehaviour
 
 	private GameObject m_poolArea;
 
+	private bool m_isInitialised = false;
+
 	public void InitialisePool(int a_poolSize)
 	{
-		m_poolSize = a_poolSize;
-		for (int i = 0; i < m_poolSize; i++)
+		if (!m_isInitialised)
 		{
-			ReturnToPool (GenerateNewBolt ());
+			m_poolArea = new GameObject ("poolArea");
+			m_poolArea.transform.position = new Vector3 (0f, -100f, 0f);
+			m_isInitialised = true;
+			m_poolSize = a_poolSize;
+			for (int i = 0; i < m_poolSize; i++)
+			{
+				ReturnToPool (GenerateNewBolt ());
+			}
 		}
 	}
 
 	public void UseBoltFromPool(GameObject startObj, GameObject endObj, bool a_autoReturn = true)
 	{
-		if (m_pool.Count < 1)
+		if (m_isInitialised)
 		{
-			ReturnToPool (GenerateNewBolt ());
+			if (m_pool.Count < 1)
+			{
+				ReturnToPool (GenerateNewBolt ());
+			}
+			float duration = Random.Range (m_lb_MinDuration, m_lb_MaxDuration);
+			LightningBoltScript lBolt = m_pool [0];
+			m_pool.RemoveAt (0);
+			lBolt.gameObject.SetActive (true);
+			lBolt.StartObject = startObj;
+			lBolt.EndObject = endObj;
+			lBolt.ChaosFactor = Random.Range(m_lb_MinChaos, m_lb_MaxChaos);
+			lBolt.Generations = Random.Range(m_lb_MinGenerations, m_lb_MaxGenerations);
+			lBolt.Duration = 0.05f;
+			if (a_autoReturn)
+			{
+				StartCoroutine(ReturnAfterTimeout(lBolt, duration));
+			}
 		}
-		float duration = Random.Range (m_lb_MinDuration, m_lb_MaxDuration);
-		LightningBoltScript lBolt = m_pool [0];
-		m_pool.RemoveAt (0);
-		lBolt.gameObject.SetActive (true);
-		lBolt.StartObject = startObj;
-		lBolt.EndObject = endObj;
-		lBolt.ChaosFactor = Random.Range(m_lb_MinChaos, m_lb_MaxChaos);
-		lBolt.Generations = Random.Range(m_lb_MinGenerations, m_lb_MaxGenerations);
-		lBolt.Duration = m_lb_MinDuration;
-		//lBolt.enabled = true;
-		if (a_autoReturn)
-		{
-			StartCoroutine(ReturnAfterTimeout(lBolt, duration));
-		}
-		//lBolt.Trigger ();
 	}
 
 	public void ReturnToPool(LightningBoltScript a_bolt)
-	{		
-		//a_bolt.enabled = false;
-		a_bolt.transform.position = m_poolArea.transform.position;
-		m_pool.Add (a_bolt);
-		a_bolt.gameObject.SetActive (false);
+	{
+		if (m_poolArea != null)
+		{
+			a_bolt.transform.position = m_poolArea.transform.position;
+			m_pool.Add (a_bolt);
+			a_bolt.gameObject.SetActive (false);
+		}
 	}
 
 	private IEnumerator ReturnAfterTimeout(LightningBoltScript a_bolt, float a_timeDuration)
@@ -69,17 +80,17 @@ public class LightningBoltPooler : MonoBehaviour
 
 	private LightningBoltScript GenerateNewBolt()
 	{
-		GameObject boltObj = Instantiate(m_lightningBoltPrefab, m_poolArea.transform) as GameObject;
+		GameObject boltObj;
+		if (m_poolArea != null)
+		{
+			boltObj = Instantiate(m_lightningBoltPrefab, m_poolArea.transform) as GameObject;
+		}
+		else
+		{
+			boltObj = Instantiate(m_lightningBoltPrefab) as GameObject;
+		}
 		LightningBoltScript lScript = boltObj.GetComponent<LightningBoltScript>();
-		//lScript.enabled = false;
 		return lScript;
 	}
-
-	private void Awake()
-	{
-		m_poolArea = new GameObject ("poolArea");
-		m_poolArea.transform.position = new Vector3 (0f, -100f, 0f);
-	}
-
 
 }
