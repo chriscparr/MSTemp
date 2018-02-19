0872db54f43e8e76975ba9110591cc3c33a47ea4
Chris <ccparr13@gmail.com>
2018-02-19 23:56:46 +0000

    Using constructors in classes that inhierit from monobehaviour is not usually a good idea. https://answers.unity.com/questions/862032/c-constructor-in-monobehaviour.html

--- Assets/Data/CaseStudyData.cs
+++ Assets/Data/CaseStudyData.cs
@@ -3,16 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
 
-public class CaseStudyData : MonoBehaviour {
-
-    public CaseStudyData()
-    {
-        ID = System.Guid.NewGuid().ToString();
-    }
-
+public class CaseStudyData : MonoBehaviour 
+{
     public string ID;
     // public string[] VideoPaths;
     public VideoClip VideoPath;
     public TextMesh Label;
-
+	
+	private void Awake()
+	{
+		ID = System.Guid.NewGuid().ToString();
+	}
 }
