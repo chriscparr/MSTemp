aa557e87bd44c2fb67abff90c5f2056116d94488
Chris Parr <chris.parr@mindshareworld.com>
2018-02-20 15:25:26 +0000

    Extended CaseStudyView - soon it will handle displaying case study data when provided with CaseStudyData

--- Assets/_Scripts/UI/CaseStudyView.cs
+++ Assets/_Scripts/UI/CaseStudyView.cs
@@ -3,32 +3,81 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CaseStudyView : MonoBehaviour {
+public class CaseStudyView : MonoBehaviour 
+{
+	[SerializeField]
+	private Button m_nextButton;
+	[SerializeField]
+	private Button m_previousButton;
+	[SerializeField]
+	private Button m_homeButton;
+	[SerializeField]
+	private Text m_titleText;
+	[SerializeField]
+	private Text m_typeText;
+	[SerializeField]
+	private Text m_otherText;
 
-    OnRailsMovement ourMover;
+	private OnRailsMovement m_ourMover;
 	// Use this for initialization
-	void Start () {
-        ourMover = Camera.main.GetComponent<OnRailsMovement>();
+
+	public void DisplayCaseStudy(CaseStudyData a_caseData)
+	{
+		m_titleText.text = a_caseData.TitleText;
+		m_typeText.text = a_caseData.CaseStudyType;
+		if (a_caseData.CaseStudyType == "TEXT")
+		{
+			m_otherText.text = a_caseData.BodyText;
+		} 
+		if (a_caseData.CaseStudyType == "VIDEO")
+		{
+			m_otherText.text = a_caseData.VideoPath;
+		}
+	}
+
+	public void ClearCaseStudy()
+	{
+		m_titleText.text = "";
+		m_typeText.text = "";
+		m_otherText.text = "";
+	}
+
+	private void Start () 
+	{
+		m_ourMover = Camera.main.GetComponent<OnRailsMovement>();
 	}
 	
-	// Update is called once per frame
-	public void GoToNextCaseStudy () {
-        if (ourMover.nodeI == 0)
-        {
-            ourMover.Initialise();
-        }
-        else
-        {
-            ourMover.GoToNextPoint();
-        }
+	private void OnEnable()
+	{
+		m_nextButton.onClick.AddListener (GoToNextCaseStudy);
+		m_previousButton.onClick.AddListener (GoToPreviousCaseStudy);
+		m_homeButton.onClick.AddListener (GoHome);
+	}
+	
+	private void OnDisable()
+	{
+		m_nextButton.onClick.RemoveListener (GoToNextCaseStudy);
+		m_previousButton.onClick.RemoveListener (GoToPreviousCaseStudy);
+		m_homeButton.onClick.RemoveListener (GoHome);
 	}
 
-    public void GoToPreviousCaseStudy() {
-        ourMover.GoToPreviousPoint();
+	// Update is called once per frame
+	private void GoToNextCaseStudy () 
+	{
+		ClearCaseStudy ();
+		m_ourMover.GoToNextPoint();
+	}
+
+	private void GoToPreviousCaseStudy() 
+	{
+		ClearCaseStudy ();
+        m_ourMover.GoToPreviousPoint();
     }
 
-    public void GoHome() {
-        ourMover.GoHome();
+	private void GoHome() 
+	{
+		ClearCaseStudy ();
+		m_ourMover.GoHome ();
         gameObject.SetActive(false);
     }
 }
