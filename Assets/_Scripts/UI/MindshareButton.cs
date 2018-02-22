c65dc6134045e6518a41b14a0eea9a42703117d8
Chris Parr <chris.parr@mindshareworld.com>
2018-02-22 16:23:15 +0000

    Created a latching button with style based on Mindshare colour palette + prefab that presents selectable options in buttons within a scrollable area

---
+++ Assets/_Scripts/UI/MindshareButton.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MindshareButton : MonoBehaviour 
+{
+	public delegate void MindshareButtonAction(string a_buttonValue);
+	public MindshareButtonAction OnSelected;
+	public MindshareButtonAction OnUnselected;
+
+	[SerializeField]
+	private Color m_msPurple;
+	[SerializeField]
+	private Color m_msWhite;
+	[SerializeField]
+	private Button m_button;
+	[SerializeField]
+	private Text m_labelText;
+
+	private bool m_isToggled = false;
+	private string m_buttonValue;
+
+	public void SetButtonValue(string a_value)
+	{
+		m_buttonValue = a_value;
+		m_labelText.text = a_value.ToUpper ();
+	}
+
+	private void Start()
+	{
+		m_button.image.color = m_msWhite;
+		m_labelText.color = m_msPurple;
+		m_button.onClick.AddListener (InvertColors);
+	}
+
+	private void InvertColors()
+	{
+		if (m_isToggled)
+		{
+			m_button.image.color = m_msPurple;
+			m_labelText.color = m_msWhite;
+			if (OnSelected != null)
+			{
+				OnSelected (m_buttonValue);
+			}
+		}
+		else
+		{
+			m_button.image.color = m_msWhite;
+			m_labelText.color = m_msPurple;
+			if (OnUnselected != null)
+			{
+				OnUnselected (m_buttonValue);
+			}
+		}
+		m_isToggled = !m_isToggled;
+	}
+}
---
+++ Assets/_Scripts/UI/ScrollingButtonSelect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollingButtonSelect : MonoBehaviour 
+{
+	[SerializeField]
+	private MindshareButton m_msButtonPrefab;
+	[SerializeField]
+	private GameObject m_contentArea;
+
+	private string[] m_selectOptions = new string[] { "Option1", "Option2", "Option3", "Option4", "Option5" };
+
+	private float m_buttonWidth = 200f;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_contentArea.GetComponent<RectTransform> ().SetSizeWithCurrentAnchors (RectTransform.Axis.Horizontal, (m_selectOptions.Length * m_buttonWidth));
+	}
+	
+
+}
