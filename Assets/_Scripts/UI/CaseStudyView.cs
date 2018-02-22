962b3821f0e9af27be967c521b5cc42182d50eb7
Chris Parr <chris.parr@mindshareworld.com>
2018-02-22 14:14:03 +0000

    Template for case study ui panels

--- Assets/_Scripts/UI/CaseStudyView.cs
+++ Assets/_Scripts/UI/CaseStudyView.cs
@@ -12,35 +12,51 @@ public class CaseStudyView : MonoBehaviour
 	[SerializeField]
 	private Button m_homeButton;
 	[SerializeField]
+	private GameObject m_introPanel;
+	[SerializeField]
 	private Text m_titleText;
 	[SerializeField]
-	private Text m_typeText;
+	private Text m_introQuestionText;
 	[SerializeField]
-	private Text m_otherText;
+	private GameObject m_contentPanel;
+	[SerializeField]
+	private Text m_bodyText;
+
+	[SerializeField]
+	private GameObject m_videoPanel;
+
+	[SerializeField]
+	private Image m_videoPlaceholder;
 
 	private OnRailsMovement m_ourMover;
-	// Use this for initialization
+
 
 	public void DisplayCaseStudy(CaseStudyData a_caseData)
 	{
 		ClearCaseStudy ();
 		m_titleText.text = a_caseData.TitleText;
-		m_typeText.text = a_caseData.CaseStudyType;
+		m_introQuestionText.text = "Is this just an introductory question, or could it be the beginning of a new path for your business?";
+		m_introPanel.SetActive(true);
+		iTween.FadeFrom (m_introPanel, iTween.Hash ("alpha",0f,"time",2f,"easetype",iTween.EaseType.easeInExpo));
+
+
 		if (a_caseData.CaseStudyType == "TEXT")
 		{
-			m_otherText.text = a_caseData.BodyText;
+			m_bodyText.text = a_caseData.BodyText;
 		} 
 		if (a_caseData.CaseStudyType == "VIDEO")
 		{
-			m_otherText.text = a_caseData.VideoPath;
+			m_bodyText.text = a_caseData.VideoPath;
 		}
 	}
 
 	public void ClearCaseStudy()
 	{
+		m_introPanel.SetActive (false);
+		m_contentPanel.SetActive (false);
 		m_titleText.text = "";
-		m_typeText.text = "";
-		m_otherText.text = "";
+		m_introQuestionText.text = "";
+		m_bodyText.text = "";
 	}
 
 	private void Start () 
