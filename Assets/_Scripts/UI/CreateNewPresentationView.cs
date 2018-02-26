6a860d81dccd8984a27b686d8bff7cbe03c0a6a5
Chris Parr <chris.parr@mindshareworld.com>
2018-02-26 15:08:02 +0000

    Making use of new differentiator box prefab for the service grid in new presentation view

--- Assets/_Scripts/UI/CreateNewPresentationView.cs
+++ Assets/_Scripts/UI/CreateNewPresentationView.cs
@@ -9,19 +9,21 @@ public class CreateNewPresentationView : MonoBehaviour
 	[SerializeField]
 	private GameObject m_buttonScrollPrefab;
 	[SerializeField]
-	private GameObject m_msToggleBoxPrefab;
+	private GameObject m_msDiffBoxPrefab;
 
-
+	private string[] m_buttonServiceLabels = new string[] {"FAST","SHOP","GROWTH","DATA","LOOP","CONTENT","AGILE","LIFE"};
 
 	[SerializeField]
 	private GameObject m_serviceButtonGrid;
 
+	private List<ServiceData> m_services = new List<ServiceData> ();
 
 
 	private PresentationData m_presentationData;
 
 	public void SetupView(PresentationData a_pData = null)
 	{
+		m_services.Clear ();
 		if (a_pData != null)
 		{
 			m_presentationData = a_pData;
@@ -30,6 +32,39 @@ public class CreateNewPresentationView : MonoBehaviour
 		{
 			m_presentationData = new PresentationData ();
 		}
+	}
 
+	private void Awake()
+	{
+		foreach (string label in m_buttonServiceLabels)
+		{
+			GameObject btnObj = Instantiate(m_msDiffBoxPrefab, m_serviceButtonGrid.transform) as GameObject;
+			MindshareButton msButton = btnObj.GetComponent<MindshareButton> ();
+			msButton.SetButtonValue (label);
+			msButton.OnSelected += ServiceButtonSelectedEventHandler;
+			msButton.OnUnselected += ServiceButtonUnselectedEventHandler;
+		}
+	}
+
+	private void ServiceButtonSelectedEventHandler(string a_selectedOption)
+	{
+		Debug.Log ("You have selected " + a_selectedOption);
+		//spawn service config panel or whatever
+
+	}
+
+	private void ServiceButtonUnselectedEventHandler(string a_unSelectedOption)
+	{
+		Debug.Log ("You have UNselected " + a_unSelectedOption);
+		/*
+		for (int i = 0; i < m_services.Count; i++)
+		{
+			if (m_services [i].ServiceName.ToUpper () == a_unSelectedOption.ToUpper ())
+			{
+				m_services.RemoveAt (i);
+				break;
+			}
+		}
+		*/
 	}
 }
