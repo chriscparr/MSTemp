af5aaf278ed9ce7c25e6f9ad3927ced42fb7c7a2
Chris Parr <chris.parr@mindshareworld.com>
2018-02-26 13:09:32 +0000

    Created new prefab + script for boxes used in create new presentation UI

---
+++ Assets/_Scripts/UI/MSAddStuffBox.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MSAddStuffBox : MonoBehaviour 
+{
+	public delegate void MSAddStuffBoxAction(string a_buttonValue);
+	public MSAddStuffBoxAction OnSelected;
+	public MSAddStuffBoxAction OnUnselected;
+
+	[SerializeField]
+	private Color m_msPurple;
+	[SerializeField]
+	private Color m_msWhite;
+	[SerializeField]
+	private Button m_button;
+	[SerializeField]
+	private Text m_labelText;
+	[SerializeField]
+	private Image m_iconImage;
+	[SerializeField]
+	private Sprite m_addIcon;
+	[SerializeField]
+	private Sprite m_removeIcon;
+
+	private bool m_isToggled = true;
+	private string m_buttonValue;
+
+	public void SetButtonValue(string a_value)
+	{
+		m_buttonValue = a_value;
+		m_labelText.text = a_value.ToUpper ();
+	}
+
+	private void Start()
+	{
+		m_button.image.color = m_msWhite;
+		m_labelText.color = m_msPurple;
+		m_button.onClick.AddListener (InvertColors);
+	}
+
+	private void InvertColors()
+	{
+		if (m_isToggled)
+		{
+			m_button.image.color = m_msPurple;
+			m_labelText.color = m_msWhite;
+			if (OnSelected != null)
+			{
+				OnSelected (m_buttonValue);
+			}
+		}
+		else
+		{
+			m_button.image.color = m_msWhite;
+			m_labelText.color = m_msPurple;
+			if (OnUnselected != null)
+			{
+				OnUnselected (m_buttonValue);
+			}
+		}
+		m_isToggled = !m_isToggled;
+	}
+}
