1413c77c629d80b9ac2b03d246ffa3736f857b27
Chris Parr <chris.parr@mindshareworld.com>
2018-02-27 16:44:08 +0000

    Constructor for serviceData sets the scale values to a sensible value and sets the edited flag, which we will rely on later

--- Assets/Data/ServiceData.cs
+++ Assets/Data/ServiceData.cs
@@ -3,6 +3,13 @@
 [System.Serializable]
 public class ServiceData
 {
+	public ServiceData()
+	{
+		InitialScale = 1.0f;
+		EditedScale = 1.0f;
+		Edited = false;
+	}
+
 	public string ServiceName;
 	public float InitialScale;
 	public float EditedScale;
