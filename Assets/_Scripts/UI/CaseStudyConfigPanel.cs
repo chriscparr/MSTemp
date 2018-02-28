0e3e72854a06a8db6de9f8641a64a2ba51be94b1
Chris Parr <chris.parr@mindshareworld.com>
2018-02-28 11:38:15 +0000

    CaseStudyConfigPanel - prefab + script created, still in the initial stages

---
+++ Assets/_Scripts/UI/CaseStudyConfigPanel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CaseStudyConfigPanel : MonoBehaviour 
+{
+	public delegate void CaseStudyConfigAction(CaseStudyData a_csData);
+	public CaseStudyConfigAction OnSaveCaseStudy;
+
+	[SerializeField]
+	private Button m_closeButton;
+	[SerializeField]
+	private Button m_textTypeButton;
+	[SerializeField]
+	private Button m_videoTypeButton;
+	[SerializeField]
+	private Button m_addVideoButton;
+	[SerializeField]
+	private Button m_saveCaseButton;
+	[SerializeField]
+	private InputField m_csTitleInputText;
+	[SerializeField]
+	private InputField m_csIntroInputText;
+	[SerializeField]
+	private InputField m_csBodyInputText;
+	[SerializeField]
+	private Text m_videoPathText;
+
+	private bool m_isVideoType = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
