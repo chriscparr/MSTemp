a334e835bbe3b31d685179cf836a3e58f76376f1
Chris Parr <chris.parr@mindshareworld.com>
2018-02-28 12:16:01 +0000

    Implemented color switching on case study type buttons (video + text)

--- Assets/_Scripts/UI/CaseStudyConfigPanel.cs
+++ Assets/_Scripts/UI/CaseStudyConfigPanel.cs
@@ -26,16 +26,86 @@ public class CaseStudyConfigPanel : MonoBehaviour
 	private InputField m_csBodyInputText;
 	[SerializeField]
 	private Text m_videoPathText;
+	[SerializeField]
+	private Color m_msPurple;
 
-	private bool m_isVideoType = true;
+	private bool m_isVideoType = false;
+	private CaseStudyData m_caseData;
 
-	// Use this for initialization
-	void Start () {
-		
+	private void Start()
+	{
+		m_textTypeButton.onClick.AddListener (TextButtonPressed);
+		m_videoTypeButton.onClick.AddListener (VideoButtonPressed);
+		m_addVideoButton.onClick.AddListener (AddVideoButtonPressed);
+		Initialise ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void Initialise(CaseStudyData a_csData = null)
+	{
+		if (a_csData != null)
+		{
+			m_caseData = a_csData;
+			m_csTitleInputText.text = m_caseData.TitleText;
+			m_csIntroInputText.text = m_caseData.IntroText;
+			m_csBodyInputText.text = m_caseData.BodyText;
+			m_videoPathText.text = "Video location : " + m_caseData.VideoPath;
+			if (m_caseData.CaseStudyType == "VIDEO")
+			{
+				VideoButtonPressed ();
+			}
+			else
+			{
+				TextButtonPressed ();
+			}
+		}
+		else
+		{
+			m_caseData = new CaseStudyData ();
+			VideoButtonPressed ();
+		}
+	}
+
+	private void VideoButtonPressed()
+	{
+		if (!m_isVideoType)
+		{
+			m_videoTypeButton.image.color = Color.white;
+			m_videoTypeButton.GetComponentInChildren<Text> ().color = m_msPurple;
+			m_textTypeButton.image.color = m_msPurple;
+			m_textTypeButton.GetComponentInChildren<Text> ().color = Color.white;
+			m_csBodyInputText.gameObject.SetActive (false);
+			m_videoPathText.gameObject.SetActive (true);
+			m_addVideoButton.gameObject.SetActive (true);
+			m_caseData.CaseStudyType = "VIDEO";
+			m_isVideoType = true;
+		}
+	}
+
+	private void TextButtonPressed()
+	{
+		if (m_isVideoType)
+		{
+			m_textTypeButton.image.color = Color.white;
+			m_textTypeButton.GetComponentInChildren<Text> ().color = m_msPurple;
+			m_videoTypeButton.image.color = m_msPurple;
+			m_videoTypeButton.GetComponentInChildren<Text> ().color = Color.white;
+			m_csBodyInputText.gameObject.SetActive (true);
+			m_videoPathText.gameObject.SetActive (false);
+			m_addVideoButton.gameObject.SetActive (false);
+			m_caseData.CaseStudyType = "TEXT";
+			m_isVideoType = false;
+		}
+	}
+
+	private void AddVideoButtonPressed()
+	{
+
+	}
+
+	private void OnDestroy()
+	{
+		m_textTypeButton.onClick.RemoveListener (TextButtonPressed);
+		m_videoTypeButton.onClick.RemoveListener (VideoButtonPressed);
+		m_addVideoButton.onClick.RemoveListener (AddVideoButtonPressed);
 	}
 }
