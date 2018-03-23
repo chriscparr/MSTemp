941e378107742774e6a84a06f64852bb56077b5e
Chris Parr <chris.parr@mindshareworld.com>
2018-03-23 10:58:28 +0000

    Linking up newly implemented UI elements

--- Assets/_Scripts/UI/LoginView.cs
+++ Assets/_Scripts/UI/LoginView.cs
@@ -30,22 +30,52 @@ public class LoginView : MonoBehaviour
 			GameObject bioTouch = Instantiate<GameObject> (m_bioTouchPrefab, null);
 			m_bioTouch = bioTouch.GetComponent<BiometricTouch> ();
 			m_bioTouch.OnTouchResult += BioTouchEventHandler;
-			m_bioTouch.RequestTouchAuth ();
 		}
-		else
-		{
-			m_bioTouch.RequestTouchAuth ();
-		}
+		m_bioTouch.RequestTouchAuth ();
     }
 
 	private void OnEnable()
 	{
 		m_TouchIDButton.onClick.AddListener (OnTouchIDButtonPressed);
+		m_pwPanelCloseBtn.onClick.AddListener (OnPanelCloseButton);
+		m_pwPanelLoginBtn.onClick.AddListener (OnPanelLoginButton);
 	}
 
 	private void OnDisable()
 	{
 		m_TouchIDButton.onClick.RemoveListener (OnTouchIDButtonPressed);
+		m_pwPanelCloseBtn.onClick.RemoveListener (OnPanelCloseButton);
+		m_pwPanelLoginBtn.onClick.RemoveListener (OnPanelLoginButton);
+	}
+
+	private void OpenEmailPWPanel()
+	{
+		m_pwPanelEmailInput.text = "";
+		m_pwPanelPWInput.text = "";
+		m_TouchIDButton.gameObject.SetActive (false);
+		m_pwPanel.SetActive (true);
+	}
+
+	private void OnPanelCloseButton()
+	{
+		m_TouchIDButton.gameObject.SetActive (true);
+		m_pwPanel.SetActive (false);
+	}
+
+	private void OnPanelLoginButton()
+	{
+		//TODO actually verify text inputs! Here are some very insecure checks in the meantime!
+		string emailCheckText = m_pwPanelEmailInput.text;
+		string passwordCheckText = m_pwPanelPWInput.text;
+		if (m_pwPanelEmailInput.text == emailCheckText && m_pwPanelPWInput.text == passwordCheckText)
+		{
+			OnPanelCloseButton ();
+			UIManager.Instance.ShowNewOrSavedView ();
+		}
+		else
+		{
+			OpenEmailPWPanel ();
+		}
 	}
 
 	private void BioTouchEventHandler(bool a_result, int a_msgCode, string a_message)
